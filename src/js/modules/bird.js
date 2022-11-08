793afb9a4d5fdaf5f4124ed19725d4cb9235e25e
Andrey Nikonov <a.einomi@gmail.com>
2022-11-08 14:03:05 +0500

    Add flying birds

---
+++ src/js/modules/bird.js
@@ -0,0 +1,100 @@
+// @ts-ignore
+import random from 'canvas-sketch-util/random';
+
+/** @typedef Params
+ * @property {number} x
+ * @property {number} y
+ * @property {{x: number; y: number}} cursor
+ * @property {CanvasRenderingContext2D} context
+ *  */
+
+class Bird {
+  /** @param {Params} params */
+  constructor({ x, y, context, cursor }) {
+    this.context = context;
+    this.cursor = cursor;
+
+    // const x = random.range(width * 0.2, width * 0.8);
+    // const y = random.range(height * 0.2, height * 0.8);
+
+    this.initialX = x;
+    this.initialY = y;
+    this.x = x;
+    this.y = y;
+
+    // acceleration
+    this.ax = 0;
+    this.ay = 0;
+
+    // velocity
+    this.vx = 0;
+    this.vy = 0;
+
+    this.minDist = 200;
+    this.pushFactor = random.range(0.05, 0.08);
+    this.pullFactor = random.range(0.01, 0.02);
+    this.dampFactor = random.range(0.9, 0.95);
+  }
+
+  drawWingStart() {
+    this.context.strokeStyle = '#dedede';
+    this.context.save();
+    this.context.beginPath();
+    this.context.moveTo(this.x, this.y);
+    this.context.translate(this.x, this.y);
+    this.context.lineWidth = 2;
+  }
+
+  drawWingEnd() {
+    this.context.stroke();
+    this.context.restore();
+  }
+
+  drawLeftWing() {
+    this.drawWingStart();
+    this.context.lineTo(random.range(-20, -10), random.range(-40, -50));
+    this.drawWingEnd();
+  }
+
+  drawRightWing() {
+    this.drawWingStart();
+    this.context.lineTo(random.range(10, 40), random.range(-20, -10));
+    this.drawWingEnd();
+  }
+
+  update() {
+    // pull force
+    let dx = this.initialX - this.x;
+    let dy = this.initialY - this.y;
+    let dd = Math.sqrt(dx * dx + dy * dy);
+
+    this.ax = dx * this.pullFactor;
+    this.ay = dy * this.pullFactor;
+
+    // push force
+    dx = this.x - this.cursor.x;
+    dy = this.y - this.cursor.y;
+    dd = Math.sqrt(dx * dx + dy * dy);
+
+    const distDelta = this.minDist - dd;
+
+    if (dd < this.minDist) {
+      this.ax += (dx / dd) * distDelta * this.pushFactor;
+      this.ay += (dy / dd) * distDelta * this.pushFactor;
+    }
+
+    this.vx += this.ax;
+    this.vy += this.ay;
+
+    this.vx *= this.dampFactor;
+    this.vy *= this.dampFactor;
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.drawLeftWing();
+    this.drawRightWing();
+  }
+}
+
+export default Bird;
---
+++ src/js/modules/sketching.js
@@ -0,0 +1,77 @@
+// @ts-ignore
+import canvasSketch from 'canvas-sketch';
+// @ts-ignore
+import random from 'canvas-sketch-util/random';
+
+import Bird from './bird';
+
+const settings = {
+  dimensions: [1080, 1080],
+  playbackRate: 'throttle',
+  animate: true,
+  fps: 20,
+  duration: 100,
+};
+
+/** @typedef SketchProps
+ * @property {CanvasRenderingContext2D} context
+ * @property {HTMLCanvasElement} canvas
+ * @property {number} width
+ * @property {number} height
+ *  */
+
+/** @param {SketchProps} args */
+function sketch({ context, canvas, width, height }) {
+  const cursor = { x: 9999, y: 9999 };
+
+  canvas.addEventListener('mousemove', onMouseMove);
+
+  /** @param {MouseEvent} event */
+  function onMouseMove(event) {
+    const x = (event.offsetX / canvas.offsetWidth) * canvas.width;
+    const y = (event.offsetY / canvas.offsetHeight) * canvas.height;
+
+    cursor.x = x;
+    cursor.y = y;
+  }
+
+  /** @type {Bird[]} */
+  const birds = [];
+
+  const circlesCount = 6;
+  const birdSize = 40;
+  let circleRadius = 27;
+  const fitRadius = birdSize;
+
+  // eslint-disable-next-line id-length
+  for (let i = 0; i < circlesCount; i += 1) {
+    const circumference = Math.PI * 2 * circleRadius;
+    const numFit = i ? Math.floor(circumference / (fitRadius * 2)) : 1;
+    const fitSlice = (Math.PI * 2) / numFit;
+
+    // eslint-disable-next-line id-length
+    for (let j = 0; j < numFit; j += 1) {
+      if (!random.chance(0.9 - i / circlesCount)) {
+        continue;
+      }
+      const theta = fitSlice * j;
+
+      const x = Math.cos(theta) * circleRadius + width / 2;
+      const y = Math.sin(theta) * circleRadius + height / 2;
+
+      birds.push(new Bird({ x, y, context, cursor }));
+    }
+
+    circleRadius += 90;
+  }
+
+  /** @param {SketchProps} args */
+  // eslint-disable-next-line no-shadow
+  return function render({ context, width, height }) {
+    context.fillStyle = '#000';
+    context.fillRect(0, 0, width, height);
+    birds.forEach((bird) => bird.update());
+  };
+}
+
+canvasSketch(sketch, settings);
